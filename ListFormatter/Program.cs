772e67c752974b92ea75710387d901d7d0d5deb5
IslamMarouf <islammarouf2012@gmail.com>
2019-01-01 13:06:58 +0200

    About To Finish

--- ListFormatter/Program.cs
+++ ListFormatter/Program.cs
@@ -35,36 +35,6 @@ namespace ListFormatter
             return y - x == 1;
         }
 
-        public static string GetRangeString(int[] arr)
-        {
-            // Return fast if array is null or contains less than 2 items
-            if (arr == null || !arr.Any()) return string.Empty;
-            if (arr.Length == 1) return arr[0].ToString();
-
-            var rangeString = new StringBuilder();
-            bool isRange = false;
-
-            for (int i = 0; i < arr.Length; i++)
-            {
-                while (i < arr.Length - 1 && arr[i] + 1 == arr[i + 1])
-                {
-                    if (!isRange) rangeString.Append($"{arr[i]}");
-                    isRange = true;
-                    i++;
-                }
-
-                if (isRange)
-                {
-                    rangeString.Append("-");
-                    isRange = false;
-                }
-
-                rangeString.Append($"{arr[i]},");
-            }
-
-            return rangeString.ToString().TrimEnd(',');
-        }
-
         public static string GetRangeString(List<int> list)
         {
             // Return fast if list is null or contains less than 2 items
@@ -80,7 +50,7 @@ namespace ListFormatter
             {
                 while (i < list.Count - 1 && list[i] + 1 == list[i + 1])
                 {
-                    if (!isRange) rangeString.Append($" # days from {list[i]}");
+                    if (!isRange) rangeString.Append($" # from {list[i]}");
                     isRange = true;
                     rangeCount++;
                     i++;
@@ -91,7 +61,7 @@ namespace ListFormatter
                     rangeString.Append(" to ");
                     isRange = false;
                     // This line is ok.....
-                    rangeString.Replace("#", $"{rangeCount + 1}");
+                    rangeString.Replace("#", $"{rangeCount + 1}" + NumberStr(rangeCount + 1));
                     // Under Development....
                     rangeString.Replace("@", $"{difference - 1}");
                     rangeCount = 0;
@@ -108,11 +78,19 @@ namespace ListFormatter
 
             rangeString.Replace(" (0) ", "");
             rangeString.Replace(" (@) ", "");
-            string temp = string.Empty;
-            temp = rangeString.ToString().TrimEnd(',');
+
+            string temp = rangeString.ToString().TrimEnd(',');
             temp += $" ({list.Count} days.)";
 
-            return temp;      // ... ... 
+            return temp; // ... ... 
+        }
+
+        public static string NumberStr(int n)
+        {
+            if (n == 1)
+                return " number";
+
+            return " numbers";
         }
     }
 }
